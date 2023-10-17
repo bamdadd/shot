810d20309fe3bf2b28ae423d6a7790a634ff698f
Bamdad Dashtban <bamdadd@gmail.com>
2023-10-17 09:23:09 +0100

    opening chatGPT

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
+use rdev::listen;
 use std::thread;
 use std::time::Duration;
-
-use rdev::listen;
-use time;
 use tray_item::{IconSource, TrayItem};
+use time;
+use color_eyre::eyre::Result;
 
+use thirtyfour::prelude::*;
+use tokio;
 mod keypress_handler;
 mod screenshot;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     // Listening to events in a separate thread so as not to block the main thread
+    color_eyre::install()?;
+    let caps = DesiredCapabilities::chrome();
+
+    let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    driver.goto("https://chat.openai.com/").await?;
+    let login_button = driver.find(By::XPath("//*[@id=\"__next\"]/div[1]/div[2]/div[1]/div/div/button[1]")).await?;
+    if (login_button.is_enabled()).await? {
+        login_button.click().await?;
+    }
 
     thread::spawn(|| {
         listen(keypress_handler::on_keypress);
